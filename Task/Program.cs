e33475f23d6d4e6b4a2a4ff0f282b45bd45cbec4
Koha <BelkoZdorovo@yandex.ru>
2022-11-13 12:48:50 +0300

    Chekpoint 1 Generation

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿Console.Clear();
+
+string[] CreateArray()
+{
+    string[] array = new string[new Random().Next(2, 11)];
+    return array;
+}
+
+string CreateRngString()
+{
+    Random generator = new Random();
+
+    int stringlen = generator.Next(1, 11);
+    int randValue;
+    string str = string.Empty;
+    char letter;
+
+    for (int i = 0; i < stringlen; i++)
+    {
+        randValue = generator.Next(0, 26);
+        letter = Convert.ToChar(randValue + 65);
+        str = str + letter;
+    }
+
+    return str;
+}
+
+void FillArray(string[] collection)
+{
+    for(int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = CreateRngString();
+    }
+}
+
+string[] arr = CreateArray();
+FillArray(arr);
+Console.Write(String.Join(", ", arr));
